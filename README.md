5e0a2c22f8a002c72cd180e899af98a4e1676993
YOOGA0809 <72486179+YOOGA0809@users.noreply.github.com>
2025-05-25 02:44:04 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # LiteFishAutoHelper
 
-自動釣魚 + 小遊戲自動化 + 自動賣魚 + 玩家靠近暫停 + 開關熱鍵
+Auto-fishing mod for Minecraft LiteFish plugin. Includes land check, minigame automation, and fish selling.
---
+++ src/main/java/com/yooga/litefish/LiteFishAutoHelperClient.java
@@ -0,0 +1,10 @@
+package com.yooga.litefish;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LiteFishAutoHelperClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("LiteFish Auto Helper loaded.");
+    }
+}
